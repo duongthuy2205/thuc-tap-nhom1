616065171157fc46fa2ee4187a20ee0784357c6d
Truong Van Dat <truongdat0406@gmail.com>
2022-05-11 23:32:52 +0700

    dattvd quan ly chuc vu

---
+++ AppQL_NhanSu/UC_ChucVu.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace AppQL_NhanSu
+{
+    partial class UC_ChucVu
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.STT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.macv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tencv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(302, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 22);
+            this.textBox2.TabIndex = 54;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(302, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 22);
+            this.textBox1.TabIndex = 53;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(540, 124);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.TabIndex = 50;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(540, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 49;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(540, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 48;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Tên Chức Vụ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 17);
+            this.label6.TabIndex = 46;
+            this.label6.Text = "Danh Sách Các Chức Vụ";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.STT,
+            this.macv,
+            this.tencv});
+            this.dataGridView1.Location = new System.Drawing.Point(66, 240);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(693, 280);
+            this.dataGridView1.TabIndex = 45;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ChooseData);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 44;
+            this.label1.Text = "Mã Chức Vụ";
+            // 
+            // STT
+            // 
+            this.STT.HeaderText = "STT";
+            this.STT.MinimumWidth = 6;
+            this.STT.Name = "STT";
+            this.STT.Width = 125;
+            // 
+            // macv
+            // 
+            this.macv.HeaderText = "Mã Chức Vụ";
+            this.macv.MinimumWidth = 6;
+            this.macv.Name = "macv";
+            this.macv.Width = 125;
+            // 
+            // tencv
+            // 
+            this.tencv.HeaderText = "Tên Chức Vụ";
+            this.tencv.MinimumWidth = 6;
+            this.tencv.Name = "tencv";
+            this.tencv.Width = 125;
+            // 
+            // UC_ChucVu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "UC_ChucVu";
+            this.Size = new System.Drawing.Size(875, 523);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn STT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn macv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tencv;
+    }
+}
---
+++ AppQL_NhanSu/UC_ChucVu.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AppQL_NhanSu.entity;
+
+namespace AppQL_NhanSu
+{
+    public partial class UC_ChucVu : UserControl
+    {
+        Model_NhanSu context = new Model_NhanSu();
+        public UC_ChucVu()
+        {
+            InitializeComponent();
+            loadcv();
+        }
+        void loadcv()
+        {
+            var cv = from chucvu in context.CHUCVUs select new { macv = chucvu.MaCV, ten = chucvu.TenCV };
+            cv.ToList();
+            int index = -1;
+            dataGridView1.Rows.Clear();
+            foreach(var item in cv)
+            {
+                dataGridView1.Rows.Add(item);
+                index++;
+                dataGridView1.Rows[index].Cells["STT"].Value = index + 1;
+                dataGridView1.Rows[index].Cells["macv"].Value = item.macv;
+                dataGridView1.Rows[index].Cells["tencv"].Value = item.ten;
+            }
+        }
+
+        void add()
+        {
+            CHUCVU chucvu = new CHUCVU();
+            chucvu.MaCV = textBox1.Text;
+            chucvu.TenCV = textBox2.Text;
+            var cvIndatabase = context.CHUCVUs.Where(s => s.MaCV == chucvu.MaCV).SingleOrDefault();
+            if(cvIndatabase == null)
+            {
+                context.CHUCVUs.Add(chucvu);
+            }
+            else
+            {
+                MessageBox.Show("Chức Vụ Đã Tồn Tại Mời Bạn Nhập Lại");
+            }
+                context.SaveChanges();
+        }
+        void edit()
+        {
+            var chucvu = dataGridView1.CurrentRow.Cells["macv"].Value.ToString();
+            CHUCVU cvIndatabase = context.CHUCVUs.Single(s => s.MaCV.Equals(chucvu));
+            if (textBox1.Text.Length > 0 && textBox1.Text != null)
+            {
+                cvIndatabase.TenCV = textBox2.Text;
+            }
+            context.SaveChanges();
+        }
+        void remove()
+        {
+            var cv = context.CHUCVUs.Where(s => s.MaCV.Equals(textBox1.Text)).SingleOrDefault();
+            if (cv != null)
+            {
+                context.CHUCVUs.Remove(cv);
+                context.SaveChanges();
+            }
+            else
+            {
+                MessageBox.Show("Chức Vụ bạn Xóa Không Tồn Tại");
+            }
+        }
+
+        private void ChooseData(object sender, DataGridViewCellEventArgs e)
+        {
+            var chucvu = dataGridView1.CurrentRow.Cells["macv"].Value.ToString();
+            CHUCVU cvIndatabase = context.CHUCVUs.Single(s => s.MaCV.Equals(chucvu));
+            textBox1.Text = cvIndatabase.MaCV;
+            textBox2.Text = cvIndatabase.TenCV;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            add();
+            loadcv();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                edit();
+                loadcv();
+                MessageBox.Show("Cập Nhật Thành Công");
+            }
+            catch
+            {
+                MessageBox.Show("Cập Nhật Thất Bại");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Bạn có muốn xóa tài khoản này không", "Thông Báo", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.Yes)
+            {
+                remove();
+                loadcv();
+            }
+        }
+    }
+}
