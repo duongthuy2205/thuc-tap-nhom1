a6b438c5505e2e0958c2709bc408fe80df84cdb6
NgaCan <thungacan99@gmail.com>
2022-05-12 17:52:40 +0700

    Add PhongBan

---
+++ AppQL_NhanSu/UC_PhongBan.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace AppQL_NhanSu
+{
+    partial class UC_PhongBan
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.stt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tenpb = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mapb = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sdt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.diachi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(525, 224);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 28);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "Tìm Kiếm";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(537, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "Xóa";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(537, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(537, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(336, 153);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(149, 22);
+            this.textBox4.TabIndex = 21;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(336, 112);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 22);
+            this.textBox3.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(336, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 22);
+            this.textBox2.TabIndex = 19;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(336, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 22);
+            this.textBox1.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(249, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Địa Chỉ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 17);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Số Điện Thoại";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Mã Phòng Ban";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Tên Phòng Ban";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.stt,
+            this.tenpb,
+            this.mapb,
+            this.sdt,
+            this.diachi});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 280);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(875, 243);
+            this.dataGridView1.TabIndex = 13;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.chosedaata);
+            // 
+            // stt
+            // 
+            this.stt.HeaderText = "STT";
+            this.stt.MinimumWidth = 6;
+            this.stt.Name = "stt";
+            this.stt.Width = 125;
+            // 
+            // tenpb
+            // 
+            this.tenpb.HeaderText = "Tên Phòng Ban";
+            this.tenpb.MinimumWidth = 6;
+            this.tenpb.Name = "tenpb";
+            this.tenpb.Width = 125;
+            // 
+            // mapb
+            // 
+            this.mapb.HeaderText = "Mã Phòng Ban";
+            this.mapb.MinimumWidth = 6;
+            this.mapb.Name = "mapb";
+            this.mapb.Width = 125;
+            // 
+            // sdt
+            // 
+            this.sdt.HeaderText = "Số Điện Thoại";
+            this.sdt.MinimumWidth = 6;
+            this.sdt.Name = "sdt";
+            this.sdt.Width = 125;
+            // 
+            // diachi
+            // 
+            this.diachi.HeaderText = "Địa Chỉ";
+            this.diachi.MinimumWidth = 6;
+            this.diachi.Name = "diachi";
+            this.diachi.Width = 125;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 260);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 17);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Danh Sách Phòng Ban";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(269, 230);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(239, 22);
+            this.textBox5.TabIndex = 27;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(537, 163);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 34);
+            this.button5.TabIndex = 28;
+            this.button5.Text = "Reload";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // UC_PhongBan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "UC_PhongBan";
+            this.Size = new System.Drawing.Size(875, 523);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tenpb;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mapb;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sdt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn diachi;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ AppQL_NhanSu/UC_PhongBan.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AppQL_NhanSu.entity;
+
+namespace AppQL_NhanSu
+{
+    public partial class UC_PhongBan : UserControl
+    {
+        Model_NhanSu context = new Model_NhanSu();
+        public UC_PhongBan()
+        {
+            InitializeComponent();
+            loadPB();
+        }
+        void loadPB()
+        {
+            var query = from phongban in context.PhongBans select new { phongban.MaPb, phongban.TenPB, phongban.DiaChi, phongban.SDTPB };
+            query.ToList();
+            int index = -1;
+            dataGridView1.Rows.Clear();
+            foreach (var item in query)
+            {
+                dataGridView1.Rows.Add();
+                index++;
+                dataGridView1.Rows[index].Cells["STT"].Value = index + 1;
+                dataGridView1.Rows[index].Cells["tenpb"].Value = item.TenPB;
+                dataGridView1.Rows[index].Cells["mapb"].Value = item.MaPb;
+                dataGridView1.Rows[index].Cells["sdt"].Value = item.SDTPB;
+                dataGridView1.Rows[index].Cells["diachi"].Value = item.DiaChi;
+            }
+        }
+        void addPB()
+        {
+           PhongBan pb = new PhongBan();
+            pb.MaPb = textBox1.Text.ToString();
+            pb.TenPB = textBox2.Text.ToString();
+            pb.SDTPB = textBox3.Text.ToString();
+            pb.DiaChi = textBox4.Text.ToString();
+            var phongb = context.PhongBans.ToList();
+            var check = context.PhongBans.Where(s => s.MaPb == pb.MaPb).SingleOrDefault();
+            if (check == null)
+            {
+                context.PhongBans.Add(pb);
+            }
+            else
+            {
+                MessageBox.Show("phòng ban đã tồn tại mời nhập lại");
+            }
+            context.SaveChanges();
+        }
+        void editPB()
+        {
+            var phongban = dataGridView1.CurrentRow.Cells["mapb"].Value.ToString();
+            PhongBan pbIndatabae = context.PhongBans.Single(s => s.MaPb.Equals(phongban));
+            if (textBox1.Text != null && textBox1.Text.Length>0)
+            {
+                pbIndatabae.TenPB =textBox2.Text;
+            }
+            pbIndatabae.SDTPB = textBox3.Text;
+            pbIndatabae.DiaChi = textBox4.Text;
+            context.SaveChanges();
+        }
+        void removePB()
+        {
+            var phongban = context.PhongBans.Where(s => s.MaPb.Equals(textBox1.Text)).SingleOrDefault();
+            if (phongban != null)
+            {
+                context.PhongBans.Remove(phongban);
+                context.SaveChanges();
+            }
+            else
+            {
+                MessageBox.Show("không tồn tại phong ban như thế");
+            }
+
+        }
+        void searchPB()
+        {
+            var lst = from pb in context.PhongBans where pb.TenPB.Contains(textBox5.Text.ToString()) select new { mapb = pb.MaPb, tenpb = pb.TenPB, sdt = pb.SDTPB, dc = pb.DiaChi };
+            int index = -1;
+            dataGridView1.Rows.Clear();
+            foreach (var item in lst)
+            {
+                dataGridView1.Rows.Add();
+                index++;
+                dataGridView1.Rows[index].Cells["STT"].Value = index + 1;
+                dataGridView1.Rows[index].Cells["tenpb"].Value = item.tenpb;
+                dataGridView1.Rows[index].Cells["mapb"].Value = item.mapb;
+                dataGridView1.Rows[index].Cells["sdt"].Value = item.sdt;
+                dataGridView1.Rows[index].Cells["diachi"].Value = item.dc;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            addPB();
+            loadPB();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                editPB();
+                loadPB();
+                MessageBox.Show("Cập Nhật Thành Công");
+            }
+            catch
+            {
+                MessageBox.Show("Cập Nhật Thất Bại");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Bạn có muốn xóa không", "", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                removePB();
+                loadPB();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            searchPB();
+        }
+
+        private void chosedaata(object sender, DataGridViewCellEventArgs e)
+        {
+            var phongban = dataGridView1.CurrentRow.Cells["mapb"].Value.ToString();
+            PhongBan pbIndatabae = context.PhongBans.Single(s => s.MaPb.Equals(phongban));
+            textBox1.Text = pbIndatabae.MaPb;
+            textBox2.Text = pbIndatabae.TenPB;
+            textBox3.Text = pbIndatabae.SDTPB;
+            textBox4.Text = pbIndatabae.DiaChi;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            loadPB();
+        }
+    }
+}
